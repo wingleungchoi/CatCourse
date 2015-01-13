cd3ca44ffa39c7b1930651cd6856356f1c269cb3
leungleungtest1 <leungleungtest1@gmail.com>
2015-01-13 11:10:22 +0800

    very basic paypal done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def front
     @user = User.new
+    flash.now[:success] = "HOHOHOHO"
   end
 
   def aboutus
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,19 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
 
+  def payurl_url(return_url)
+    values = {
+      :business => 'wingleungchoi-facilitator@gmail.com',
+      :cmd => "_xclick",
+      :return => return_url,
+      :amount => "1",
+      :item_name => "cat_training_course",
+      :item_number => "001",
+      :quantity => "1",
+    }
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?"+values.map{|k,v| "#{k}=#{v}"}.join("&")
+  end
+
   private
   def downcase_email
     self.email = self.email.downcase if self.email.present?
